9ce5ff2625c5adcb8086575675a4c6ec740732a2
Alex <guzmancarrilloa@yahoo.com>
2023-09-24 21:35:26 -0600

    Solved problem 2

--- Problem1.js
+++ Problem1.js
@@ -3,16 +3,16 @@ that solves the following problem; return a list of all the integers in the rang
 that do not appear in nums. */
 
 function whereAreThey(nums){
-    const uniqueValues = new Set(nums);
+    const uniqueValues = new Set(nums); //Sets ensure only unique values are saved
     let missingNums = [];
 
     for (let i = 1; i < nums.length; i++){
-        if (!uniqueValues.has(i)){
+        if (!uniqueValues.has(i)){ //If the number is not in the set, add it to the answer list
             missingNums.push(i);
         }
     }
     return missingNums;
 }
 
-let listex = [4, 3, 2, 7, 8, 2, 3, 1];
-console.log(whereAreThey(listex));
+let listEx = [4, 3, 2, 7, 8, 2, 3, 1];
+console.log(whereAreThey(listEx));
--- Problem2.js
+++ Problem2.js
@@ -3,3 +3,20 @@ problem; return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order. */
 
+function twoNumbersOneTarget (nums, target){
+    const hashM = {}; // Pseudo-dictionary object to store numbers and their keys
+    
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        
+        // If the difference is in the dictionary, return its key and the current key
+        if (Object.hasOwn(hashM, diff)) {
+            return [hashM[diff], i];
+        }
+        hashM[nums[i]] = i;
+    }
+}
+
+const numsEx = [2, 7, 11, 15];
+const targetEx = 9;
+console.log(twoNumbersOneTarget(numsEx, targetEx));
